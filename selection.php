50cb9217be4161951509b3557dca00031db60371
Jonah Eng <engjonah@gmail.com>
2023-01-01 23:05:57 -0500

    update link

--- selection.php
+++ selection.php
@@ -1,1 +1,1 @@
-<p><a href="https://live-caption.herokuapp.com/main.php?recog=zh&trans=en&bgcolor=black&size=50&weight=900&color=pink&st_color=black&st_width=2&v_align=bottom">standard link</a></p>
+<p><a href="https://live-caption.herokuapp.com/main.php?recog=zh&trans=en&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">standard link</a></p>
