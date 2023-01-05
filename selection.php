97d808e3b561f32b9df81eac47093ad1755e6900
Jonah Eng <engjonah@gmail.com>
2023-01-04 21:49:40 -0500

    selection footer and link addition

--- selection.php
+++ selection.php
@@ -1,1 +1,15 @@
-<p><a href="https://live-caption.herokuapp.com/main.php?recog=zh&trans=en&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">standard link</a></p>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Caption Options</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <p><a href="https://live-caption.herokuapp.com/main.php?recog=zh&trans=en&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">Chinese</a></p>
+        <p><a href="https://live-caption.herokuapp.com/main.php?recog=en&trans=zh&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">English</a></p>
+    </body>
+    <footer>
+        <p>Author: Jonah Eng</p>
+        <p>Adopted From: <a href="https://sayonari.coresv.com/ninshikiChan/"></p>
+    </footer>
+</html>
