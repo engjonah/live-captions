d7b66b0273fbbecf29d5a1261373cbb07e08f890
Jonah Eng <engjonah@gmail.com>
2023-01-04 22:08:46 -0500

    reorg and hyperlink

--- selection.php
+++ selection.php
@@ -5,11 +5,16 @@
         <link rel="stylesheet" href="./style.css">
     </head>
     <body>
-        <p><a href="https://live-caption.herokuapp.com/main.php?recog=zh&trans=en&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">Chinese</a></p>
-        <p><a href="https://live-caption.herokuapp.com/main.php?recog=en&trans=zh&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">English</a></p>
+        <div id="container">
+            <div id="header"></div>
+            <div id="body">
+                <p><a href="https://live-caption.herokuapp.com/main.php?recog=zh&trans=en&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">Chinese</a></p>
+                <p><a href="https://live-caption.herokuapp.com/main.php?recog=en&trans=zh&bgcolor=black&size=50&weight=900&color=white&st_color=black&st_width=2&v_align=bottom">English</a></p>
+            </div>
+            <div id="footer">
+                <p>Author: Jonah Eng</p>
+                <p>Adopted From: <a href="https://sayonari.coresv.com/ninshikiChan/">https://sayonari.coresv.com/ninshikiChan/</a></p>
+            </div>
+        </div>
     </body>
-    <footer>
-        <p>Author: Jonah Eng</p>
-        <p>Adopted From: <a href="https://sayonari.coresv.com/ninshikiChan/"></p>
-    </footer>
 </html>
